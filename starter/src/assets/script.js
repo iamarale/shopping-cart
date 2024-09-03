a090bfb36bafc1ce3d3fa812158bd2fb2a7865f8
iamarale <ismailarales@gmail.com>
2024-09-03 09:33:19 -0500

    refactor(products): added images and updated Id

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,40 +1,42 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+
 const products = [
+  /* Images provided in /images folder. All images from Unsplash.com
+   - cherry.jpg by Mae Mu
+   - orange.jpg by Mae Mu
+   - strawberry.jpg by Allec Gomes
+*/
   {
-    name: "Slice of Fruit Pie",
+    name: "Cherry",
     price: 8.99,
     quantity: 0,
     productId: 1,
+    image: "../images/cherry.jpg",
+  },
+  {
+    name: "Orange",
+    price: 8.99,
+    quantity: 0,
+    productId: 2,
+    image: "../images/orange.jpg",
+  },
+  {
+    name: "Strawberry",
+    price: 8.99,
+    quantity: 0,
+    productId: 3,
+    image: "../images/strawberry.jpg",
+  },
+  {
+    name: "Cherry",
+    price: 8.99,
+    quantity: 0,
+    productId: 4,
     image:
       "https://images.unsplash.com/photo-1476887334197-56adbf254e1a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
-  {
-    name: "Calzone",
-    price: 10.0,
-    quantity: 0,
-    productId: 2,
-    image:
-      "https://images.unsplash.com/photo-1707080032705-ec2df78fd395?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Chicken Scarpariello",
-    price: 14.99,
-    quantity: 0,
-    productId: 3,
-    image:
-      "https://assets.bonappetit.com/photos/59c94a8f3b3bf713cb63808f/1:1/w_2560%2Cc_limit/chicken-scarpariello.jpg",
-  },
-  {
-    name: "Baked Stuffed Eggplant with Italian Sausage",
-    price: 14.99,
-    quantity: 0,
-    productId: 3,
-    image:
-      "https://www.foodandwine.com/thmb/zuTM2DyGbvdso_PSGZmPIwqgNgQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/0895_191203_dupree_fw_seo_bakedstuffedeggplant_16352-4f71168c25ba4bafabc21c2ebb08848e.jpg",
-  },
-];
 
-/* Create 3 or more product objects using object literal notation 
+  /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
    - name: name of product (string)
    - price: price of product (number)
@@ -42,21 +44,41 @@ const products = [
    - productId: unique id for the product (number)
    - image: picture of product (url string)
 */
-
-/* Images provided in /images folder. All images from Unsplash.com
-   - cherry.jpg by Mae Mu
-   - orange.jpg by Mae Mu
-   - strawberry.jpg by Allec Gomes
-*/
+  {
+    name: "Calzone",
+    price: 10.0,
+    quantity: 0,
+    productId: 5,
+    image:
+      "https://images.unsplash.com/photo-1707080032705-ec2df78fd395?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Chicken Scarpariello",
+    price: 14.99,
+    quantity: 0,
+    productId: 6,
+    image:
+      "https://assets.bonappetit.com/photos/59c94a8f3b3bf713cb63808f/1:1/w_2560%2Cc_limit/chicken-scarpariello.jpg",
+  },
+  {
+    name: "Baked Stuffed Eggplant with Italian Sausage",
+    price: 14.99,
+    quantity: 0,
+    productId: 7,
+    image:
+      "https://www.foodandwine.com/thmb/zuTM2DyGbvdso_PSGZmPIwqgNgQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/0895_191203_dupree_fw_seo_bakedstuffedeggplant_16352-4f71168c25ba4bafabc21c2ebb08848e.jpg",
+  },
+];
 
 /* Declare an empty array named cart to hold the items in the cart */
+const cart = [];
 
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
-
+const addProductToCart = function () {};
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
